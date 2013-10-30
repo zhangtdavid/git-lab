62911007953ccc9eec5541b38bf78333cc47cc4c
David Zhang <davidtzh@usc.edu>
2013-10-30 13:05:50 -0700

    Added rule for Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,8 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
-            System.out.println(i);
+            if(i % 3 == 0)
+            	System.out.println(i + ": " + "Fizz");
         }
     }
 }
