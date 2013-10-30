36831d6c01dd101d4cfb1d7f23044a77cb4641f2
Ryan Choi <ryanchoi@usc.edu>
2013-10-30 13:10:20 -0700

    Added rule for Buzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,8 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
-            if(i % 3 == 0)
-            	System.out.println(i + ": " + "Fizz");
+            if( i % 5 == 0)
+                System.out.println("Buzz");
         }
     }
 }
